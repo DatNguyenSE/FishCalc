e5921f1762391259e2405416a4ead4073808f0b7
DatNguyenSE <datnt05se@gmail.com>
2025-12-07 16:42:44 +0700

    refactor(db): update property type DateTime to DateOnly from  SalaryPayment entity

--- FishCalc.Web/DTOs/SalaryProcessDto.cs
+++ FishCalc.Web/DTOs/SalaryProcessDto.cs
@@ -2,10 +2,10 @@ using System;
 
 namespace FishCalc.Web.DTOs
 {
-    public class SalaryPaymentDto
+    public class SalaryProcessDto
     {
         public int PaymentId { get; set; }
-        public DateTime PaymentDate { get; set; }
+        public DateOnly PaymentDate { get; set; }
         public int FishId { get; set; }
         public int UnitId { get; set; }
         public decimal TotalQuantityProcessed { get; set; }
